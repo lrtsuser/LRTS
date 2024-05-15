6cb5cfa2aae99e423d4aaa5cb7cc8ff8e5905a11
lrtsuser <issta2024lrts@gmail.com>
2024-05-15 14:17:05 -0400

    no mth in omin

--- scripts/build_omin.py
+++ scripts/build_omin.py
@@ -3,58 +3,24 @@ import pandas as pd
 import multiprocessing as mp
 import glob
 import const
-import extract_test_result
 import evaluation.eval_const as eval_const
 
 
-def convert_method_status(status):
-    if status in [const.FAILED, const.REGRESSION]:
-        return eval_const.FAIL
-    return eval_const.PASS
-
-
-def get_test_class_outcome(df_slice):
-    # test class outcome is computed based on test method outcomes
-    outcomes = df_slice["method_status"].values.tolist()
-    if eval_const.FAIL in outcomes:
-        # if any method fails, the class is marked failed
-        return eval_const.FAIL
-    return eval_const.PASS
-
-
-def stage_statistics(method_df, class_df):
-    # total_suite_duration: sum of suite duration by executed suites
-    stage_duration_by_suite_sum = method_df.drop_duplicates(subset=["suite_classname"])["suite_duration"].sum()
-    # total testing duration by summing method duration
-    stage_duration_by_method_sum = method_df["method_duration"].sum()
-
-    # num_pass_method: number of executed passed test methods
-    num_pass_method = len(method_df[method_df["method_status"] == eval_const.PASS].index)
-
-    # num_fail_method: number of executed failed test methods
-    num_fail_method = len(method_df[method_df["method_status"] == eval_const.FAIL].index)
+def stage_statistics(class_df):
+    # total testing duration by summing test class duration
+    stage_duration = class_df["duration"].sum()
 
     # test class level pass and fail
     num_pass_class = len(class_df[class_df["outcome"] == eval_const.PASS].index)
     num_fail_class = len(class_df[class_df["outcome"] == eval_const.FAIL].index)
     num_trans_class = (class_df["outcome"] - class_df["last_outcome"]).abs().sum()
 
-    return [stage_duration_by_suite_sum, stage_duration_by_method_sum,
-        num_pass_class, num_fail_class, num_trans_class,
-        num_pass_method, num_fail_method]
+    return [stage_duration, num_pass_class, num_fail_class, num_trans_class]
 
 
 def process_test_result(project, pr_name, build_id):
     """collect statistics for all stages of a build"""
     print(project, pr_name, build_id)
-    df = pd.read_csv(os.path.join(
-        eval_const.trdir, project, 
-        f"{pr_name}_build{build_id}", eval_const.TEST_REPORT_CSV))
-    
-    # filter invalid test names and skipped tests
-    df = extract_test_result.filter_tests(project, df)    
-    # convert method status
-    df["method_status"] = df["method_status"].apply(lambda x: convert_method_status(x))
     stage_class_csv_files = glob.glob(
         os.path.join(eval_const.trdir, project, 
                      f"{pr_name}_build{build_id}", "stage_*", eval_const.TEST_CLASS_CSV))
@@ -62,12 +28,8 @@ def process_test_result(project, pr_name, build_id):
     rows = []
     for stage_class_csv_file in stage_class_csv_files:
         stage = stage_class_csv_file.split("/")[-2].replace("stage_", "")
-        if project.startswith(const.TVM):
-            stage_method_df = df if stage == "single" else df[df["stage_id"].str.contains(stage)]
-        else:
-            stage_method_df = df if stage == "single" else df[df["stage_id"] == stage]
         stage_class_df = pd.read_csv(stage_class_csv_file)
-        stage_stats = stage_statistics(stage_method_df, stage_class_df)
+        stage_stats = stage_statistics(stage_class_df)
         rows.append([project, pr_name, build_id, stage] + stage_stats)
     return rows
 
@@ -83,15 +45,17 @@ def get_omin_file():
     ret = [l for ls in ret for l in ls]
     ret = pd.DataFrame(ret, columns=[
         "project", "pr_name", "build_id", "stage_id",
-        "stage_duration_by_suite_sum", "stage_duration_by_method_sum",
-        "num_pass_class", "num_fail_class", "num_trans_class",
-        "num_pass_method", "num_fail_method",
+        "stage_duration", "num_pass_class", "num_fail_class", "num_trans_class",
     ])
     df = pd.merge(df, ret, "left", on=["project", "pr_name", "build_id"])
     
     # remove useless cols, now all builds in this csv have diff data
     df = df.drop(
-        labels=["trunk_sha_timestamp_sec", "has_trunk_head_diff_data"],
+        labels=[
+            "trunk_sha_timestamp_sec", "has_trunk_head_diff_data"
+            # info from raw test report
+            "ts_duration", "passcount", "failcount", "skipcount",
+            ],
         axis=1)
     # sort by project, and time from oldest to newest
     df = df.sort_values(by=["project", "build_timestamp"], ascending=True)
